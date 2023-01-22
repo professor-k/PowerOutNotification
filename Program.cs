2ca382bfa3cb07c8ab0c0a5d2d978a32e9e57604
Andriy Kvasnytsya <andriy@kvasnytsya.name>
2023-01-22 17:07:59 +0200

    Sending out notifications

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using System.Collections.Concurrent;
 using PowerOutNotification;
-
-var timeStamps = new ConcurrentDictionary<string, DateTime>();
+using Telegram.Bot;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var timeStamps = new ConcurrentDictionary<string, DateTime>();
+var telegramBotKey = builder.Configuration["TelegramBotKey"];
+
 builder.Services.AddSingleton(timeStamps);
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddSingleton(new TelegramBotClient(telegramBotKey));
 
 var app = builder.Build();
 app.UseHttpsRedirection();
@@ -13,6 +17,7 @@ app.UseHttpsRedirection();
 app.MapGet("/powerOn", (string address) =>
 {
     timeStamps[address] = DateTime.UtcNow;
+    return DateTime.UtcNow.ToLongTimeString();
 });
 
 app.Run();
--- Worker.cs
+++ Worker.cs
@@ -1,18 +1,24 @@
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 
 namespace PowerOutNotification;
 
 public class Worker : IHostedService, IDisposable
 {
     private readonly ConcurrentDictionary<string, DateTime> timeStamps;
+    private readonly TelegramBotClient bot;
+    private readonly string channelId;
     private readonly ConcurrentDictionary<string, bool> lastStates = new();
-    private readonly TimeSpan threshold = TimeSpan.FromSeconds(10);
-    private readonly TimeSpan runSpan = TimeSpan.FromSeconds(10);
+    private readonly TimeSpan threshold = TimeSpan.FromSeconds(30);
+    private readonly TimeSpan runSpan = TimeSpan.FromSeconds(30);
     private Timer? timer;
 
-    public Worker(ConcurrentDictionary<string, DateTime> timeStamps)
+    public Worker(ConcurrentDictionary<string, DateTime> timeStamps, TelegramBotClient bot, IConfiguration config)
     {
         this.timeStamps = timeStamps;
+        this.bot = bot;
+        channelId = config["TelegramChannelId"];
     }
 
     public Task StartAsync(CancellationToken stoppingToken)
@@ -21,14 +27,17 @@ public class Worker : IHostedService, IDisposable
         return Task.CompletedTask;
     }
 
-    private void DoWork(object? state)
+    private async void DoWork(object? state)
     {
         foreach (var timeStamp in timeStamps)
         {
             bool currentState = DateTime.UtcNow - timeStamp.Value < threshold;
             if (!lastStates.TryGetValue(timeStamp.Key, out var lastState) || currentState != lastState)
             {
-                // TODO: logger.Log(LogLevel.Information, $"{timeStamp.Key}: {currentState}");
+                await bot.SendTextMessageAsync(
+                    new ChatId(channelId),
+                    text: $"{timeStamp.Key}: Електрика {(currentState ? "зʼявилась" : "зникла")}",
+                    cancellationToken: CancellationToken.None);
             }
             lastStates[timeStamp.Key] = currentState;
         }
